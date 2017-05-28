0d551cea0a228a6078959a807bea99169b7c9895
Arnaud Lachaume <arnaud.lachaume@maestrano.com>
2017-05-28 18:54:22 -0400

    dockerize minio

---
+++ 2.0/healthcheck.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+# This script must exit with:
+# 0 on success
+# 1 on error
+
+# Abort if no healthcheck
+[ "$NO_HEALTHCHECK" = "true" ] && exit 0
+
+# Monitor login page
+curl -f http://localhost:9000/minio/login || exit 1
